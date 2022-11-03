d504d135e29967bde717038724101e9e1e9872d3
iiiidaaa <iida_kazunori@cyberagent.co.jp>
2022-11-04 01:40:52 +0900

    fix output format

--- Readme.md
+++ Readme.md
@@ -5,6 +5,7 @@ docker build -t run-shodan -f cmd/run-shodan/Dockerfile .
 ```
 
 ## Run
+formatには、`json`,`text`を指定してください
 ```shell
-docker run -e SHODAN_KEY={YOUR_API_KEY} run-shodan {target}
+docker run -e SHODAN_KEY={YOUR_API_KEY} run-shodan {format} {target}
 ```
--- cmd/run-shodan/main.go
+++ cmd/run-shodan/main.go
@@ -12,16 +12,57 @@ import (
 
 func main() {
 	args := os.Args
-	if len(args) != 2 {
-		log.Fatalf("target must be set")
-
+	if len(args) != 3 {
+		log.Fatalf("Usage: go run main.go {text|json}, {target}")
+	}
+	if !(args[1] == "text") && !(args[1] == "json") {
+		log.Fatalf("The format must be text or json")
 	}
 	client := shodan.NewEnvClient(nil)
-	searchHost(client, args[1])
+	found, err := searchHost(client, args[2])
+	if err != nil {
+		log.Fatalf("error occured: %+v", err)
+	}
+	switch args[1] {
+	case "text":
+		printTextGetHostResult(found)
+	case "json":
+		printJsonGetHostResult(found)
+	}
 
 }
 
-func searchHost(client *shodan.Client, query string) {
+func printJsonGetHostResult(hostMatch *shodan.HostMatch) error {
+	j, err := json.Marshal(hostMatch)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(j))
+	return nil
+}
+
+func printTextGetHostResult(hostMatch *shodan.HostMatch) {
+
+	for _, m := range hostMatch.Matches {
+		fmt.Printf("product: %+v\n", m.Product)
+		fmt.Printf("Hostnames: %+v\n", m.Hostnames)
+		fmt.Printf("IP: %+v\n", m.IP)
+		fmt.Printf("Port: %+v\n", m.Port)
+		fmt.Printf("ISP: %+v\n", m.ISP)
+		fmt.Printf("CPE: %+v\n", m.CPE)
+		fmt.Printf("ASN: %+v\n", m.ASN)
+		fmt.Printf("Banner: %+v\n", m.Banner)
+		fmt.Printf("Link: %+v\n", m.Link)
+		fmt.Printf("Transport: %+v\n", m.Transport)
+		fmt.Printf("Domains: %+v\n", m.Domains)
+		fmt.Printf("DeviceType: %+v\n", m.DeviceType)
+		fmt.Printf("Location: %+v\n", m.Location)
+		fmt.Printf("ShodanData: %+v\n\n", m.ShodanData)
+	}
+	fmt.Printf("total: %v\n", hostMatch.Total)
+}
+
+func searchHost(client *shodan.Client, query string) (*shodan.HostMatch, error) {
 	options := &shodan.HostQueryOptions{
 		Query:  query,
 		Facets: "",
@@ -30,14 +71,10 @@ func searchHost(client *shodan.Client, query string) {
 	}
 	found, err := client.GetHostsForQuery(context.Background(), options)
 	if err != nil {
-		log.Panic(err)
+		return nil, err
 	}
-	j, err := json.Marshal(found)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println(string(j))
 
+	return found, nil
 }
 
 func getDNS(client *shodan.Client, hostnames []string) {
